7b2cb1c3504b11164121ef6b16091950e337b61c
GustavoVieck <gustavo.dores@sptech.school>
2025-12-10 21:03:26 -0300

    Mudando ANotação Schedule

--- slack_hafutech/hafutech/src/main/java/sptech/school/hafutech/HafutechApplication.java
+++ slack_hafutech/hafutech/src/main/java/sptech/school/hafutech/HafutechApplication.java
@@ -2,14 +2,12 @@ package sptech.school.hafutech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import sptech.school.hafutech.controller.NotificacaoController;
 
 @SpringBootApplication
-@EnableScheduling
 public class HafutechApplication {
     public static void main(String[] args) {
         SpringApplication.run(HafutechApplication.class, args);
+
     }
 }
--- slack_hafutech/hafutech/src/main/java/sptech/school/hafutech/config/NotificacaoSchedule.java
+++ slack_hafutech/hafutech/src/main/java/sptech/school/hafutech/config/NotificacaoSchedule.java
@@ -1,13 +1,16 @@
 package sptech.school.hafutech.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import sptech.school.hafutech.message.ModeloMensagem;
 import sptech.school.hafutech.service.NotificacaoService;
 
 @Component
-public class NotificacaoScheduler {
+@EnableScheduling
+public class NotificacaoSchedule {
 
     @Autowired
     private NotificacaoService notificacaoService;
@@ -17,11 +20,14 @@ public class NotificacaoScheduler {
 
     @Scheduled(cron = "0 */1 * * * *")
     public void executar() {
+        notificacaoService.enviarNotificacao(modeloMensagem.enviouMensagem());
         notificacaoService.enviarMensagemPorRegiao(modeloMensagem.mensagemAlertaNorte());
         notificacaoService.enviarMensagemPorRegiao(modeloMensagem.mensagemAlertaNordeste());
         notificacaoService.enviarMensagemPorRegiao(modeloMensagem.mensagemAlertaSul());
         notificacaoService.enviarMensagemPorRegiao(modeloMensagem.mensagemAlertaSudeste());
         notificacaoService.enviarMensagemPorRegiao(modeloMensagem.mensagemAlertaCentroOeste());
+
+           System.out.println("Enviando dados..");
     }
 }
 
